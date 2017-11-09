28a6698d2616de24b52aab9c8b369c26c9cca757
Mathias <mrl@cureit.dk>
2017-11-09 09:58:09 +0100

    Første udkast af playgame lavet

--- DoYouKnowMe/PlayGame/PlayGameViewController.swift
+++ DoYouKnowMe/PlayGame/PlayGameViewController.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2017 Brian Christensen. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlayGame: UIViewController {
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
