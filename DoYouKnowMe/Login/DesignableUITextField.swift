7a7b818d24da0ad949dcf772fd7b658dec44ab04
mathiaslarsen <info@opengate.dk>
2017-11-16 00:41:05 +0100

    navngivninger

--- DoYouKnowMe/Login/DesignableUITextField.swift
+++ DoYouKnowMe/Login/DesignableUITextField.swift
@@ -18,7 +18,7 @@ class DesignableUITextField: UITextField {
         return textRect
     }
 
-    @IBInspectable var leftImage: UIImage? {
+    @IBInspectable var kasseBillede: UIImage? {
         didSet {
             updateView()
         }
@@ -31,7 +31,7 @@ class DesignableUITextField: UITextField {
 
     @IBInspectable var leftPadding: CGFloat = 0
 
-    @IBInspectable var color: UIColor = UIColor.lightGray {
+    @IBInspectable var hintFarve: UIColor = UIColor.lightGray {
         didSet {
             updateView()
         }
@@ -49,13 +49,13 @@ class DesignableUITextField: UITextField {
             imageView.widthAnchor.constraint(equalToConstant: frame.height)
             ])
         
-        if let image = leftImage {
+        if let image = kasseBillede {
             leftViewMode = UITextFieldViewMode.always
             let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
             imageView.image = image
 
             // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
-            imageView.tintColor = color
+            imageView.tintColor = hintFarve
             leftView = imageView
         } else {
             leftViewMode = UITextFieldViewMode.never
@@ -63,7 +63,7 @@ class DesignableUITextField: UITextField {
         }
 
         // Placeholder text color
-        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: color])
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: hintFarve])
     }
 }
 
