cad242684042c2dc411333837035579215512d21
Mathias <mrl@cureit.dk>
2017-11-19 17:56:45 +0100

    En del af playgame logik er klart, der mangler stadig en del

--- DoYouKnowMe/PlayGame/PlayGameViewController.swift
+++ DoYouKnowMe/PlayGame/PlayGameViewController.swift
@@ -11,9 +11,10 @@ import UIKit
 class PlayGameViewController: UIViewController {
     
     private var turnNumber = 0
+    private var noOfQuestions = 0
+    private var currentQuestion = 0
     private var names: [String] = []
-    private var questions: [String] = []
-    private var startingPlayerIndex: Int = 99
+    private var currentPlayerIndex: Int = 99
     
     
     @IBOutlet var Gameview: UIView!
@@ -25,9 +26,21 @@ class PlayGameViewController: UIViewController {
     
     
     override func viewWillAppear(_ animated: Bool) {
+        //names.append(Game.sharedInstance.player1Name)
+        //names.append(Game.sharedInstance.player2Name)
+        names.append("Mathias")
+        names.append("Brian")
+        Answer1.setTitle(names[0], for: .normal)
+        Answer2.setTitle(names[1], for: .normal)
+        noOfQuestions = Game.sharedInstance.allQuestions.count
+        print("No of questions selected \(noOfQuestions)")
+        findStartingPlayer()
+        updateView(currentQuestion: Game.sharedInstance.allQuestions[currentQuestion].text, currentPlayer: names[currentPlayerIndex])
         
-       // Answer1.setTitle(names[0], for: .normal)
-       // Answer2.setTitle(names[1], for: .normal)
+       
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
         
     }
     override func viewDidLoad() {
@@ -42,33 +55,41 @@ class PlayGameViewController: UIViewController {
     }
     
     func findStartingPlayer() -> String{
-        let diceRoll = Int(arc4random_uniform(1))
+        let diceRoll = Int(arc4random_uniform(2))
+        print("Diceroll: \(diceRoll)")
         let playerName = names[diceRoll]
-        self.startingPlayerIndex = diceRoll
-        let alert = UIAlertController(title: "Starting player found",
-                                      message: "Starting player is " + playerName,
-                                      preferredStyle: .alert)
+        self.currentPlayerIndex = diceRoll
         
-        let saveAction = UIAlertAction(title: "Okay",
-                                       style: .default)
-        alert.addAction(saveAction)
-        present(alert, animated: true)
         return playerName
     }
     func updateView(currentQuestion: String, currentPlayer: String){
-        self.turnLabel.text = "Player turn: " + currentPlayer
-        self.questionLabel.text = currentQuestion
+            self.turnLabel.text = "Player turn: " + currentPlayer
+            self.questionLabel.text = currentQuestion
+        
+        
+        
+        
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
+    func checkQuestionAnswered(questionToCheck: Int)-> Bool{
+        if(Game.sharedInstance.allQuestions[questionToCheck].p1Answer != "" || Game.sharedInstance.allQuestions[questionToCheck].p2Answer != ""){
+            return true
+        }
+        else {
+            return false
+        }
+        
+    }
+    func resolveNextTurn(){
+        
+        while currentQuestion != noOfQuestions {
+            if checkQuestionAnswered(questionToCheck: currentQuestion) == true{
+                currentQuestion = currentQuestion + 1
+                updateView(currentQuestion: Game.sharedInstance.allQuestions[currentQuestion].text, currentPlayer: names[currentPlayerIndex])
+            }
+        }
+        }
+            
+        
+    }
+
