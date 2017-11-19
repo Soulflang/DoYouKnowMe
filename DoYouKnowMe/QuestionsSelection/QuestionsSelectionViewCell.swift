068efb890e0cd10cce67fd05b575550bcfc35c07
Morten Due Christiansen <mdue84@gmail.com>
2017-11-19 09:56:00 +0100

    Når man klikker på et spørgsmål kommer  der en alert box, der viser hele spørgsmålet

--- DoYouKnowMe/QuestionsSelection/QuestionsSelectionViewCell.swift
+++ DoYouKnowMe/QuestionsSelection/QuestionsSelectionViewCell.swift
@@ -15,31 +15,39 @@ class QuestionsSelectionViewCell: UITableViewCell {
     @IBOutlet weak var cbSelected: CheckBox!
     
     var delegate: UIViewController?
+    
     var index: Int?{
         willSet {
            cbSelected.isChecked = Game.sharedInstance.allQuestions[newValue!].selected
         }
         
     }
-    
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        
-        
+    
     }
     
 
     @IBAction func checkBoxPressed(_ sender: CheckBox) {
-        print(sender.isChecked)
+        print("er checkbox checked: \(sender.isChecked)")
         Game.sharedInstance.allQuestions[index!].selected =  sender.isChecked
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        //super.setSelected(selected, animated: animated)
+        if(!selected){
+            return
+        }
+        let alert = UIAlertController(title: "Spørgsmål",
+                                        message: txtText.text,
+                                        preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Okay",
+                                       style: .default)
+        alert.addAction(saveAction)
+        delegate?.present(alert, animated: true)
     }
     
 }
